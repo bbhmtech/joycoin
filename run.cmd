e4b50b94524232c015c8c83cd86f274376298e01
Yiffyi Jia <daniel_sy@live.com>
2024-05-31 22:05:32 +0800

    optimize: debug run script

--- run.cmd
+++ run.cmd
@@ -1,7 +1,6 @@
-@echo off
 cd /d %~dp0
 set CGO_ENABLED=1
 set PATH=%PATH%;D:\msys64\mingw64\bin
-go build -gcflags="all=-N -l" -o main.exe .\cmd\main
-main.exe
+go build -gcflags="all=-N -l" -o %~n1.exe %~1
+%~n1.exe
 pause
